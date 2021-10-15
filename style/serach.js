4372fdab140ba2852b11b03a3f6aa0cb8456add6
Farhad Danish <farhaddanish22@gmail.com>
2021-10-15 19:45:33 +0430

    navbar position change

--- style/serach.js
+++ style/serach.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
-    console.log('fajdsfjaskd')
+
     $('#search').click(function (e) {
-         
+
         e.preventDefault();
         $('.menu-item').toggleClass('hide-item');
         $('.search-form').toggleClass('active-serach-form');
         $('.serach-dropdown').toggleClass('hide-dropdown');
-        $('.input-serach').attr('autofocus','on');
-        
+        // $('.input-serach').attr('autofocus','on');
 
-        console.log('fasdhfhas')
+
+
     });
 });
